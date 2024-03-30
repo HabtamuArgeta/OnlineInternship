0ff989db13ed86eb4e0af84fa1df6b08484c776b
habtamu <argetahabtamu8@gmail.com>
2024-03-31 00:35:58 +0300

    firstCommit

---
+++ Servieces/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using InternshipDotCom.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InternshipDotCom.Servieces
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var admin = new IdentityRole("admin");
+            admin.NormalizedName = "admin";
+
+            var applicant = new IdentityRole("applicant");
+            applicant.NormalizedName = "applicant";
+
+            var InternshipCordinator = new IdentityRole("InternshipCordinator");
+            InternshipCordinator.NormalizedName = "InternshipCordinator";
+
+            var organization = new IdentityRole("organization");
+            organization.NormalizedName = "organization";
+
+            builder.Entity<IdentityRole>().HasData(admin, applicant, InternshipCordinator, organization);
+        }
+    }
+}
